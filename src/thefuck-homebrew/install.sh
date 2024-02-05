6ae8905e20d7ab12c28f681a6d99dadbdec1cd07
Georg Ofenbeck - taaofge1 <georg.ofenbeck@swisscom.com>
2024-02-05 14:58:25 +0100

    first draft for thefuck featur

---
+++ src/thefuck-homebrew/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -i
+
+set -e
+
+source ./library_scripts.sh
+
+
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.0"
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/homebrew-package:1.0.7" \
+    --option package='thefuck' --option version="$VERSION"
+
+echo 'Attempting to append to .bashrc/.zshrc/.bash_profile'
+updaterc 'eval $(thefuck --alias)'
+
+echo 'Done!'
+
