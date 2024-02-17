2f86518afe3bf0203259abf955f7c08804343665
Georg Ofenbeck - taaofge1 <georg.ofenbeck@swisscom.com>
2024-02-17 18:13:23 +0100

    removed the sudo commands

--- src/lazygit-linuxbinary/install.sh
+++ src/lazygit-linuxbinary/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 
 set -e
-sudo apt-get install -y curl
+apt-get install -y curl
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 if [[ $(uname -m) == "x86_64" ]]; then
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -12,4 +12,4 @@ else
     exit 1
 fi
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+install lazygit /usr/local/bin
