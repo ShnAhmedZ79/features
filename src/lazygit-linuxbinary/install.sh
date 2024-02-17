1b07c7ec74ec4194d0f48a7d9706ee1dd95df6a6
Georg Ofenbeck - taaofge1 <georg.ofenbeck@swisscom.com>
2024-02-17 14:30:18 +0100

    added lazygit (binary or homebrew)

---
+++ src/lazygit-linuxbinary/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -i
+
+set -e
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+if [[ $(uname -m) == "x86_64" ]]; then
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+elif [[ $(uname -m) == "aarch64" ]]; then
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_arm64.tar.gz"
+else
+    echo "Unsupported architecture"
+    exit 1
+fi
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
