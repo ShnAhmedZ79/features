86b3f8bc6f6cd203894caddd93309c88d7fac8e3
Georg Ofenbeck - taaofge1 <georg.ofenbeck@swisscom.com>
2024-02-17 18:08:50 +0100

    removed dependency and doing a curl install

--- src/lazygit-linuxbinary/install.sh
+++ src/lazygit-linuxbinary/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 
 set -e
-
+sudo apt-get install -y curl
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 if [[ $(uname -m) == "x86_64" ]]; then
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
