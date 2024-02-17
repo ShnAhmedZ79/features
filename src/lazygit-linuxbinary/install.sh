9b56bcde476dd830adcfbb4901447add4ae61c16
Georg Ofenbeck - taaofge1 <georg.ofenbeck@swisscom.com>
2024-02-17 19:12:56 +0100

    yet another attempt

--- src/lazygit-linuxbinary/install.sh
+++ src/lazygit-linuxbinary/install.sh
@@ -1,26 +1,28 @@
 #!/bin/bash -i
 
 set -e
-
-
 source ./library_scripts.sh
-
-
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.5.0"
+ensure_nanolayer nanolayer_location "v0.4.29"
 
-clean_download  "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" /tmp/lazygit_version
-LAZYGIT_VERSION=$(cat /tmp/lazygit_version | grep -Po '"tag_name": "v\K[^"]*')
-rm -f /tmp/lazygit_version
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.4" \
+    --option packages='curl,ca-certificates'
+
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 if [[ $(uname -m) == "x86_64" ]]; then
-    clean_download "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" lazygit.tar.gz 
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 elif [[ $(uname -m) == "aarch64" ]]; then
-    clean_download "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_arm64.tar.gz" lazygit.tar.gz 
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_arm64.tar.gz"
 else
     echo "Unsupported architecture"
     exit 1
